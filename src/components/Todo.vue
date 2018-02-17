6e42fc9eb70946f9e8d28744ed43892b15e2b3ab
angrha <angrha@gmail.com>
2018-02-17 21:36:42 +0700

    add navbar, and todo comp

---
+++ src/components/Todo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-container style="height: 80px; border: 1px solid #eee;">
+      <el-form class="flx-td">
+        <el-form-item >
+          <el-input v-model="newTodo" class="in-td" placeholder="What needs to be done?"></el-input>
+        </el-form-item>
+      </el-form>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newTodo: ''
+    }
+  }
+}
+</script>
+
+<style>
+.flx-td {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10% 0% 0% 25%;
+}
+.in-td {
+  width: 200%;
+  border: solid 3px #eee;
+  border-radius: 3px;
+}
+</style>
---
+++ src/vuex/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+// import axios from 'axios'
+// import router from '../router'
+
+Vue.use(Vuex)
+
+// const fancyTodo = 'auth-fancy-todo'
+// const baseUrl = 'http://localhost:3000/api'
+
+const store = new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  }
+})
+
+export default store
